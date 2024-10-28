55a1d3f4eb37ea20bd27dd869a86d14c12a0e83c
Daniel Trejos <tmdaneil@gmail.com>
2024-10-28 11:08:02 -0500

    creacion del blueprint del api con una rutabasica y registro

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+
 from .site.routes import site_bp
+from .api.routes import api_bp
+
 
 # Funcion para crear la app inciada por run.py aqui se deben cargar las configuracions
 def create_app():
@@ -8,5 +11,6 @@ def create_app():
 
     # Registro de blueprints
     app.register_blueprint(site_bp)
+    app.register_blueprint(api_bp)
 
     return app
--- myapp/api/routes.py
+++ myapp/api/routes.py
@@ -1,3 +1,12 @@
 from flask import Blueprint
 
 # Blueprint para la api del sitio web
+api_bp = Blueprint('api', __name__,url_prefix='/api')
+
+# Rutas del api
+
+@api_bp.route('/getdata')
+def getdata():
+    # Simulación de un llamado a una API externa
+    # Aquí debería ir el código para hacer el llamado a la API externa
+    return {'message': 'Data retrieved successfully'}
